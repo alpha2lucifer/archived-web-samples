1204b6899dfd927430837ebca0f7b7f7fa929020
Chandan <97820157+alpha2lucifer@users.noreply.github.com>
2023-02-02 22:23:41 +0530

    Update Webpage.js

--- folder05/Webpage.js
+++ folder05/Webpage.js
@@ -1,5 +1,1 @@
-//Made By Soubhagya
-
-alert ('IT IS MY FIRST WEBSITE🤩🤩🤩 I AM FEELING REALLY GOOD THAT SOLOLEARN GIVING A SUPER PLATFORM TO OUR DREAMS')
-
-alert ('According To all of your suggestion. I have bought updates in this Website...I have Fixed the device view issue...Changed Heading Font to Orbitron...I have made this responsive to for every devices...')
+// no js😉
