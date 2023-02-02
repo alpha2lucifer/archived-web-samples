aac4564475b8dfd72f2aee7dde509e962eb9bd5b
Chandan <97820157+alpha2lucifer@users.noreply.github.com>
2023-02-02 22:02:18 +0530

    sample05

---
+++ folder05/Webpage.js
@@ -0,0 +1,5 @@
+//Made By Soubhagya
+
+alert ('IT IS MY FIRST WEBSITE🤩🤩🤩 I AM FEELING REALLY GOOD THAT SOLOLEARN GIVING A SUPER PLATFORM TO OUR DREAMS')
+
+alert ('According To all of your suggestion. I have bought updates in this Website...I have Fixed the device view issue...Changed Heading Font to Orbitron...I have made this responsive to for every devices...')
